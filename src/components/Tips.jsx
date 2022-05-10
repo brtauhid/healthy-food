919cae398e41ac53d86b895b294e7216a9866888
brtauhid <br.tauhid@gmail.com>
2022-05-11 04:42:22 +0600

    tips section 2

--- src/components/Tips.jsx
+++ src/components/Tips.jsx
@@ -4,6 +4,10 @@ import icon1 from "../img/spoon.png";
 const Container = styled.div`
   height: 670px;
   background-color: #6a4aa1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const TipsTitle = styled.h1`
@@ -11,30 +15,38 @@ const TipsTitle = styled.h1`
   font-size: 48px;
   text-align: center;
   padding-top: 45px;
+  margin-bottom: -50px;
 `;
 const TipsContainer = styled.div`
   display: flex;
-  margin-top: 30px;
-  padding: 0 100px;
+  margin-top: 100px;
+  padding: 0 50px;
 `;
 const TipsContent = styled.div`
   flex: 1;
+  margin-left: 20px;
 `;
 const SingleTipsContent = styled.div`
   display: flex;
+  margin-left: 10px;
 `;
 const TipsIcon = styled.img`
   width: 15%;
   height: 15%;
 `;
-const TipsInfo = styled.div``;
+const TipsInfo = styled.div`
+  margin-left: 20px;
+  width: 80%;
+`;
 
 const TipsInfoTitle = styled.h1`
   font-size: 20px;
   font-weight: 700;
+  color: white;
+  margin-bottom: 30px;
 `;
 const TipsInfoDescription = styled.p`
-  font-size: 16px;
+  font-size: 14px;
   font-style: italic;
   color: white;
 `;
