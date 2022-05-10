bfeb7996a296aacc1dfad585370fd3a8f97634dc
brtauhid <br.tauhid@gmail.com>
2022-05-11 01:11:12 +0600

    hero area

---
+++ src/components/Hero.jsx
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import heroBg from "../../src/img/heroBg.jpeg";
+
+const Container = styled.div`
+  height: calc(100vh - 70px);
+  background-color: yellow;
+  display: flex;
+  align-items: center;
+  background-image: url(${heroBg});
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+
+const Left = styled.div`
+  width: 50%;
+  flex: 3;
+`;
+const Right = styled.div`
+  width: 50%;
+  flex: 2;
+`;
+
+const Title = styled.div`
+  font-size: 130px;
+  font-weight: 700;
+`;
+const SubTitle = styled.div`
+  font-size: 18px;
+  font-weight: 700;
+`;
+
+const Desc = styled.p`
+  font-size: 16px;
+  width: 60%;
+  margin: 30px 0;
+`;
+
+const Btn = styled.button`
+  font-size: 20px;
+  font-weight: bold;
+  padding: 10px 25px;
+  border-radius: 5px;
+  border: none;
+`;
+
+const Hero = () => {
+  return (
+    <Container>
+      <Left>left</Left>
+      <Right>
+        <Title>health</Title>
+        <SubTitle>Time to Get Healthy & Active</SubTitle>
+        <Desc>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est
+          laborum.
+        </Desc>
+        <Btn>READ MORE</Btn>
+      </Right>
+    </Container>
+  );
+};
+
+export default Hero;
